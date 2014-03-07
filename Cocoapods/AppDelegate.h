32e660fa93eb35876a24b610c6dd252172989c5b
EDUARDO LOMBARDI <eduardolomb@gmail.com>
2014-03-07 11:44:08 -0300

    empty project

---
+++ Cocoapods/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Cocoapods
+//
+//  Created by Eduardo Lombardi on 07/03/14.
+//  Copyright (c) 2014 Eduardo Lombardi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
